4edf0aaad5f98231393b392a6d37e8d3957771a1
Oliver Bocanegra Pérez <olbope@fp.insjoaquimmir.cat>
2022-04-26 20:16:20 +0200

    a

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 module.exports = {
     // Option 1
-    'uri': process.env.DB_URI,
+    'uri': process.env.DB_URI || "mongodb+srv://Oliver:tarda1234@soundtalk.eoijz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     // Option 2
     'driver': process.env.DB_DRIVER || 'mongodb',
     'host': process.env.DB_HOST || 'localhost',
